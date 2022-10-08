3f8d2387c1b3a27188ac6e50ba4eaecb76329d3e
Jayesh <jayeshshrivastava810@gmail.com>
2022-10-08 20:33:16 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Header from './components/header/Header';
+import Header from './components/01header/Header';
+import Home from './components/02home/Home';
 
 function App() {
   return (
     <>
       <Header/>
+      <Home/>
     </>
   );
 }
